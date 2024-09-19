48c6342e1521a7f69ae3906df9762ef9a875a96e
Tarun Mitruka <mitruka@ibb.uni-stuttgart.de>
2024-09-19 14:56:03 +0200

    fix

--- ikarus/finiteelements/mechanics/enhancedassumedstrains.hh
+++ ikarus/finiteelements/mechanics/enhancedassumedstrains.hh
@@ -275,9 +275,16 @@ protected:
     const auto& numberOfNodes = underlying().numberOfNodes();
     const auto C              = underlying().materialTangentFunction(par);
 
+    decltype(auto) LMat = [this]() -> decltype(auto) {
+      if constexpr (std::is_same_v<ScalarType, double>)
+        return [this]() -> Eigen::MatrixXd& { return L_; }();
+      else
+        return Eigen::MatrixX<ScalarType>{};
+    }();
+
     auto calculateForceContribution = [&]<typename EAST>(const EAST& easFunction) {
       typename EAST::DType D;
-      calculateDAndLMatrix(easFunction, par, D, L_);
+      calculateDAndLMatrix(easFunction, par, D, LMat, dx);
 
       const auto disp    = Dune::viewAsFlatEigenVector(uFunction.coefficientsRef());
       const auto geo     = underlying().localView().element().geometry();
@@ -292,7 +299,7 @@ protected:
           force.template segment<Traits::worlddim>(Traits::worlddim * i) += bopI.transpose() * stresses * intElement;
         }
       }
-      force -= L_.transpose() * D.inverse() * Rtilde;
+      force -= LMat.transpose() * D.inverse() * Rtilde;
     };
     easVariant_(calculateForceContribution);
   }
